72cf818a3de738996f84d4cccb1de968dcfbc8be
Tom <tom@noogen.net>
2013-03-31 17:39:56 -0500

    Release 1.0.3 commit.

--- src/Phun/Properties/AssemblyInfo.cs
+++ src/Phun/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.676")]
-[assembly: AssemblyFileVersion("1.0.0331.74")]
+[assembly: AssemblyVersion("1.0.3.02")]
+[assembly: AssemblyFileVersion("1.0.0331.01")]
 [assembly: InternalsVisibleTo("Phun.Tests")]
