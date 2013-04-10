fdf8ad59042db565fb3b456b341ee2d45adecdcb
Noogen <tom@noogen.net>
2013-04-10 00:21:30 -0500

    Add web helpers.

--- src/Phun/Properties/AssemblyInfo.cs
+++ src/Phun/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.167")]
-[assembly: AssemblyFileVersion("1.0.0409.77")]
+[assembly: AssemblyVersion("1.0.3.172")]
+[assembly: AssemblyFileVersion("1.0.0410.04")]
 [assembly: InternalsVisibleTo("Phun.Tests")]
